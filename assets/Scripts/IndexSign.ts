d65865501f19e66c85fd70331a861e815d72a05f
LoJiking <779434002@qq.com>
2022-02-17 22:02:44 +0800

    update Prefabs

---
+++ assets/Scripts/IndexSign.ts
@@ -0,0 +1,14 @@
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+  onLoad() {
+    const sceneName = cc.director.getScene()?.name;
+    const index = sceneName.replace("Game", "");
+    this.node.getChildByName("Text").getComponent(cc.Label).string = `第${index}关`;
+  }
+
+  start() {}
+
+  // update (dt) {}
+}
