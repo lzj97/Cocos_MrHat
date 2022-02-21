e245500003072c7234b6666251be74da1a1fd6d8
LZJ <779434002@qq.com>
2022-02-21 14:24:55 +0800

    update UI prefab

--- assets/Scripts/Controller.ts
+++ assets/Scripts/Controller.ts
@@ -1,5 +1,6 @@
 import global from "./utils/global";
 import GlobalAudio from "./GlobalAudio";
+import Toast from "./utils/Toast";
 
 const { ccclass, property } = cc._decorator;
 @ccclass
@@ -8,7 +9,7 @@ export default class NewClass extends cc.Component {
   speed: number = 200;
 
   @property(cc.Integer)
-  jumpSpeed: number = 800;
+  jumpSpeed: number = 1200;
 
   @property(cc.Boolean)
   ContinuousJump: boolean = true;
@@ -35,10 +36,12 @@ export default class NewClass extends cc.Component {
     const Left = this.node.getChildByName("Left");
     Left.on(cc.Node.EventType.TOUCH_START, () => (this.leftLongPress = true), this);
     Left.on(cc.Node.EventType.TOUCH_END, () => (this.leftLongPress = false), this);
+    Left.on(cc.Node.EventType.TOUCH_CANCEL, () => (this.leftLongPress = false), this);
 
     const Right = this.node.getChildByName("Right");
     Right.on(cc.Node.EventType.TOUCH_START, () => (this.rightLongPress = true), this);
     Right.on(cc.Node.EventType.TOUCH_END, () => (this.rightLongPress = false), this);
+    Right.on(cc.Node.EventType.TOUCH_CANCEL, () => (this.rightLongPress = false), this);
 
     const Jump = this.node.getChildByName("Jump");
     Jump.on(cc.Node.EventType.TOUCH_START, this.jumpAction, this);
@@ -92,34 +95,44 @@ export default class NewClass extends cc.Component {
     this.GlobalAudio.playPlayerJumpEffect();
   }
 
-  help() {}
   reload() {
     const sceneName = cc.director.getScene()?.name;
     cc.director.loadScene(sceneName);
   }
   pause() {
-    const PauseOptions = this.node.getChildByName("PauseOptions");
-    PauseOptions.active = true;
+    const PauseDialog = this.node.getChildByName("PauseDialog");
+    PauseDialog.active = true;
   }
   goMain() {
     cc.director.loadScene("Main");
   }
   play() {
-    const PauseOptions = this.node.getChildByName("PauseOptions");
-    PauseOptions.active = false;
+    const PauseDialog = this.node.getChildByName("PauseDialog");
+    PauseDialog.active = false;
   }
   mute() {
-    const Phonation = cc.find("Canvas/UI/PauseOptions/Phonation");
+    const Phonation = cc.find("Canvas/UI/PauseDialog/Phonation");
     Phonation.active = false;
-    const Mute = cc.find("Canvas/UI/PauseOptions/Mute");
+    const Mute = cc.find("Canvas/UI/PauseDialog/Mute");
     Mute.active = true;
     this.GlobalAudio.mute();
   }
   phonation() {
-    const Phonation = cc.find("Canvas/UI/PauseOptions/Phonation");
+    const Phonation = cc.find("Canvas/UI/PauseDialog/Phonation");
     Phonation.active = true;
-    const Mute = cc.find("Canvas/UI/PauseOptions/Mute");
+    const Mute = cc.find("Canvas/UI/PauseDialog/Mute");
     Mute.active = false;
     this.GlobalAudio.phonation();
+  }
+  help() {
+    const TipsDialog = this.node.getChildByName("TipsDialog");
+    TipsDialog.active = true;
+  }
+  closeHelp() {
+    const TipsDialog = this.node.getChildByName("TipsDialog");
+    TipsDialog.active = false;
+  }
+  playAdv() {
+    Toast("该功能暂未完成");
   }
 }
