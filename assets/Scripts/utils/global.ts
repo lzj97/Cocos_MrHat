78640bd45359204c53c07f3a0a5fbc2d5bf2c0d9
LZJ <779434002@qq.com>
2022-02-22 16:53:49 +0800

    add tips content

--- assets/Scripts/utils/global.ts
+++ assets/Scripts/utils/global.ts
@@ -1,4 +1,12 @@
 export default {
   MUTE: "MUTE",
   PASSED_COUNT: "PASSED_COUNT",
+  tips: [
+    "连续点击多次跳跃按钮跳过去",
+    "这都过不了，应该是出BUG了",
+    "29",
+    "手残的还是看广告跳过吧",
+    "按住钥匙",
+    "暂停时仍然可以移动",
+  ],
 };
