e123fb1fa706c6635f0dfd44ad229f15d1524237
LZJ <779434002@qq.com>
2022-02-24 15:28:17 +0800

    fix rigidbody penetration err

--- assets/Scripts/SceneScripts/Level7.ts
+++ assets/Scripts/SceneScripts/Level7.ts
@@ -9,12 +9,16 @@ export default class NewClass extends cc.Component {
   wallOriginY: number = 0;
   touchOriginY: number = 0;
 
+  PlayerRigidBody: cc.RigidBody = null;
+
   onLoad() {
     const Controller = cc.find("Canvas/Controller");
     if (Controller) {
       const Script: Conscroller = Controller.getComponent("Controller");
       Script.ContinuousJump = false;
     }
+
+    this.PlayerRigidBody = cc.find("Canvas/Player").getComponent(cc.RigidBody);
 
     this.Wall.on(
       cc.Node.EventType.TOUCH_START,
@@ -28,6 +32,9 @@ export default class NewClass extends cc.Component {
       cc.Node.EventType.TOUCH_MOVE,
       (e) => {
         this.Wall.y = e.touch.getLocationY() - this.touchOriginY + this.wallOriginY;
+        const v = this.PlayerRigidBody.linearVelocity;
+        v.y += 0.000001;
+        this.PlayerRigidBody.linearVelocity = v;
       },
       this
     );
